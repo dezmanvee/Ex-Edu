47fa4434d2610e5c9cd901ebb15361c2b997f407
Bamidele Ademokoya <bamyonline@gmail.com>
2023-08-06 15:18:01 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,138 @@
+// Toggle button
+
+const toggleBtn = document.querySelector('.toggle-btn')
+const linksContainer = document.querySelector('.links-container')
+const links = document.querySelector('.links')
+
+toggleBtn.addEventListener('click', () => {
+    const linksContainerHeight = linksContainer.getBoundingClientRect().height
+    const linksHeight = links.getBoundingClientRect().height
+
+    if (linksContainerHeight === 0) {
+        linksContainer.style.height = `${linksHeight}px`
+    }
+    else {
+        linksContainer.style.height = 0
+    }
+})
+
+// Date for footer
+
+const date = document.querySelector('.date')
+date.innerHTML = new Date().getFullYear()
+
+
+//Scroll Behavior
+window.addEventListener('scroll', () => {
+    const scrollBtnTop = document.querySelector('.scroll-top')
+    const navbar = document.querySelector('.nav')
+    const linksHeight = links.getBoundingClientRect().height
+    const pageHeight = window.scrollY
+    if (pageHeight > linksHeight) {
+        navbar.classList.add('fixed-nav')
+    }
+    else {
+        navbar.classList.remove('fixed-nav')
+    }
+
+    //For the Scroll top button display
+    if (pageHeight > 1000) {
+        scrollBtnTop.classList.add('show-scroll-top')
+    }
+    else {
+        scrollBtnTop.classList.remove('show-scroll-top')
+    }
+})
+
+
+//Links Controls
+const scrollLinks = document.querySelectorAll('.scroll-links')
+
+scrollLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+        // e.preventDefault()
+        linksContainer.style.height = 0;
+    })
+})
+// ==================================
+//CARDS AND SERVICE
+// =================================
+
+//Import module from data.js
+
+import {data} from './data.js'
+const cardsContainer = document.querySelector('.cards-ctn')
+const cardBtnsContainer = document.querySelector('.card-btns')
+window.addEventListener('DOMContentLoaded', () => {
+
+    displayCards(data)
+    displayButtons()
+})
+
+
+// Displays all cards in html
+function displayCards(array){
+
+    const cards = array.map(item => {
+        return `<div class="card">
+        <div class="card-img">
+            <img src="${item.image}" alt="logo">
+        </div>
+        <div class="card-details">
+            <h4 class="program">${item.program}</h4>
+            <p>${item.description}</p>
+            <div class="pricing">
+                <p class="in-word">pricing <span class="right-arrow"><i class="bx bxs-right-arrow"></i></span></p>
+                <p class="in-figure">$${item.price}</p>
+            </div>
+        </div>
+    </div>`
+    }).join('')
+    cardsContainer.innerHTML = cards
+}
+
+
+// Button display and functionality
+function displayButtons() {
+    //Getting available buttons from data
+function programButtons() {
+    const subjects = data.map(item=> item.subject)
+    const buttons = ['all']
+    subjects.filter(subject => {
+        if (!buttons.includes(subject)){
+            buttons.push(subject)
+        }
+    })
+    return buttons
+}
+
+//display all buttons in html
+const availableButtons = programButtons().map(button => {
+    return `<button class="card-btn" data-subject="${button}">${button}</button>`
+}).join('')
+cardBtnsContainer.innerHTML = availableButtons
+const cardBtns = document.querySelectorAll('.card-btn')
+
+
+// Filter Card Buttons
+cardBtns.forEach(cardBtn => {
+    cardBtn.addEventListener('click', (e) => {
+        const subject = e.currentTarget.dataset.subject
+        //filtering cards by subjects
+        const filteredCards = data.filter(card => {
+            if (card.subject === subject) {
+                return card
+            }
+        })
+        if (subject === 'all') {
+            displayCards(data)
+        }
+        else {
+            displayCards(filteredCards)
+        }
+    })
+    
+})
+
+}
+
---
+++ data.js
@@ -0,0 +1,74 @@
+const data = [
+    {
+      subject: "physics",
+      description: "Physics 101: Introduction to Mechanics",
+      price: 25,
+      program: "Checkpoint",
+      image: "./images/phy_1.jpg"
+    },
+    {
+      subject: "chemistry",
+      description: "Chemistry 101: Basic Principles of Chemistry",
+      price: 30,
+      program: "IGCSE",
+      image: "./images/chm_1.jpg"
+    },
+    {
+      subject: "math",
+      description: "Mathematics 101: Algebra and Geometry",
+      price: 20,
+      program: "WAEC",
+      image: "./images/math_1.jpg"
+    },
+    {
+      subject: "coding",
+      description: "Coding 101: Introduction to Programming",
+      price: 15,
+      program: "Checkpoint",
+      image: "./images/coding_1.jpg"
+    },
+    {
+      subject: "physics",
+      description: "Physics 201: Electromagnetism and Optics",
+      price: 35,
+      program: "IGCSE",
+      image: "./images/phy_2.jpg"
+    },
+    {
+      subject: "chemistry",
+      description: "Chemistry 201: Organic Chemistry",
+      price: 32,
+      program: "Checkpoint",
+      image: "./images/chm_2.jpg"
+    },
+    {
+      subject: "math",
+      description: "Mathematics 201: Calculus and Trigonometry",
+      price: 28,
+      program: "WAEC",
+      image: "./images/math_2.jpg"
+    },
+    {
+      subject: "coding",
+      description: "Coding 201: Data Structures and Algorithms",
+      price: 20,
+      program: "IGCSE",
+      image: "./images/coding_2.jpg"
+    },
+    {
+      subject: "physics",
+      description: "Physics 301: Quantum Mechanics",
+      price: 40,
+      program: "WAEC",
+      image: "./images/phy_3.jpg"
+    },
+    {
+      subject: "math",
+      description: "Mathematics 301: Differential Equations",
+      price: 30,
+      program: "Checkpoint",
+      image: "./images/math_3.jpg"
+    }
+  ];
+
+  export {data};
